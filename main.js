7ce37c122a9275342e4a5aa877360eafde2306c3
AFaraazK <92804285+AFaraazK@users.noreply.github.com>
2022-08-06 10:36:27 -0400

    Symbol Swap each click

--- main.js
+++ main.js
@@ -1,24 +1,40 @@
+let pc = new Player("X");
+
 // Gameboard object (module)
 // add eventListeners to each square to listen for click
 // renderBoard() - loops through array, fills in the pieces
 const Gameboard = (function(){
+    board = [];
     const squares = document.querySelectorAll(".square");
     squares.forEach(square => {
         square.addEventListener('click', function(){
-            square.innerHTML = "X"
+            pc.playerClick(square);
         });
     });    
 
     return {};
 })();
 
-
 // Game Object (module)
 // checkWinner() - checks if any player has 3 in a row
 //      -> checks if every space is filled, make that a tie
 //      -> checks if array of their spaces matches any of the win conditions
 const Game = (function(){
+    let currentSymbol = "X";
+    let circleTurn = false;
+    function swapTurns(){
+        if(circleTurn){
+            circleTurn = false;
+            this.currentSymbol = "X";
 
+        } else {
+            circleTurn = true;
+            this.currentSymbol = "O";
+        }
+        console.log(circleTurn);
+    }
+
+    return{swapTurns,currentSymbol};
 })();
 
 // Player Object  (factory function)
@@ -26,6 +42,14 @@ const Game = (function(){
 //      -> check that space isn't already filled (index = '')
 //      -> keeps track of each symbol in a separate array
 
-const Player = function(){
-
+function Player(symbol){
+    let ownership = [];
+    this.symbol = symbol;
+    function playerClick(square){
+        ownership.push(square.id);
+        square.innerHTML = Game.currentSymbol;
+        console.log(ownership);
+        Game.swapTurns();
+    }
+    return {playerClick,symbol,ownership};
 };
