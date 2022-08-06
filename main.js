0c7da3488c748a0b3b18bc71c53d5d353b3fdee7
AFaraazK <92804285+AFaraazK@users.noreply.github.com>
2022-08-06 09:40:26 -0400

    psuedo code

---
+++ main.js
@@ -0,0 +1,23 @@
+// Gameboard object (module)
+// add eventListeners to each square to listen for click
+// renderBoard() - loops through array, fills in the pieces
+const Gameboard = (function(){
+
+})();
+
+// Game Object (module)
+// checkWinner() - checks if any player has 3 in a row
+//      -> checks if every space is filled, make that a tie
+//      -> checks if array of their spaces matches any of the win conditions
+const Game = (function(){
+
+})();
+
+// Player Object  (factory function)
+// clickBox() - fill box with that player's symbol
+//      -> check that space isn't already filled (index = '')
+//      -> keeps track of each symbol in a separate array
+
+const Player = function(){
+
+};
